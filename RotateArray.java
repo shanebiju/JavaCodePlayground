41537412c1adbd72f41fb9c90b10a813953f3337
Shane Biju <shanegarritsen@gmail.com>
2024-09-26 21:57:09 +0530

    rotate array by k positions

---
+++ RotateArray.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class RotateArray {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements in the array:");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        System.out.println("Enter the value of k:");
+        int k = sc.nextInt();
+
+        RotateArray rotateArray = new RotateArray();
+        rotateArray.rotate(nums, k);
+
+        System.out.println("Array after rotation:");
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+        sc.close();
+    }
+
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
